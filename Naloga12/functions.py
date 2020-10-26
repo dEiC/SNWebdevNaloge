c9243e4aca58009d47454b117933fdf3a0a28b17
dEiC <nightmare.dejan@gmail.com>
2020-10-26 14:45:16 +0100

    Naloga19

--- Naloga12/functions.py
+++ Naloga12/functions.py
@@ -1,38 +1,63 @@
 import json
 import datetime
 import random
+import os, sys
+
+class Results():
+    def __init__(self, score, player_name, date, score_file):
+        self.score = score
+        self.player_name = player_name
+        self.date = date
+
+    @classmethod
+    def get_score_list(cls, self):
+        with open("score_list.txt", "r") as score_file:
+            score_list = json.loads(score_file.read())
+        return score_list
+
+    @classmethod
+    def save (cls):
+
+
+        score_file = os.path.abspath("score_list.txt")
+        if os.path.exists(score_file):
+            file = open("score_list.txt", "a")
+            print(file.__dict__)
+            file.write(json.dumps(score_file))
+            file.close()
+        else:
+            file = open("score_list.txt", "x")
+            file.write(json.dumps(score_file))
+            file.close()
+    @classmethod
+    def sort_list(cls, score_list):
+        return sorted(score_list, key = lambda i: i['score'])
 
 def play_game():
     secret = random.randint(1, 30)
     guess = None
-    attempts = 0
-    score_list = get_score_list()
+    score = 0
+    score_list = []
 
     while guess != secret:
-        
+        score_list = Results.get_score_list()
         guess = int(input("Guess the secret number (between 1 and 30): "))
-        attempts += 1
+        score += 1
 
         if guess == secret:
+            print("You've guessed it - congratulations! It's number " + str(secret))
+            print("Attempts needed: " + str(score))
+            player_name = input("Please enter your name: ")
+            score_list = Results.save(score_list)
+            """
             score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
             with open("score_list.txt", "w") as score_file:
                 score_file.write(json.dumps(score_list))
-
-            print("You've guessed it - congratulations! It's number " + str(secret))
-            print("Attempts needed: " + str(attempts))
+"""
             break
         elif guess > secret:
             print("Your guess is not correct... try something smaller")
         elif guess < secret:
             print("Your guess is not correct... try something bigger")
 
-    
-
-def get_score_list():
-    with open("score_list.txt", "r") as score_file:
-        score_list = json.loads(score_file.read())
-    return score_list
-
-def sort_list(score_list):
-    return sorted(score_list, key = lambda i: i['attempts'])
 
--- Naloga12/main.py
+++ Naloga12/main.py
@@ -1,9 +1,12 @@
 import datetime
 import json
 import random
+import os, sys
+import importlib
 from functions import *
 
-score_list = get_score_list()
+
+score_list = Results.get_score_list()
 
 while True:
     selection = input("Would you like to A) Play a new game, B) See the best scores, C) quit?")
@@ -11,10 +14,13 @@ while True:
     if selection == "A":
         play_game()
     elif selection == "B":
-        for score_list in get_score_list():
-            print(str(score_list["attempts"]) + " attemps, date: " + score_list.get("date"))
-    else:
-        score_list = sort_list(score_list)
+        score_list = Results.get_score_list()
+        score_list = Results.sort_list(score_list)
         print("Top scores are: ")
         for list_item in score_list[:3]:
-            print("- Attempts: " + str(list_item["attempts"]) + " Date: " + list_item["date"])
+            print("- Attempts: " + str(list_item["score"]) + " Date: " + list_item["date"])
+
+
+    else:
+        break
+
